2e0cdefdebb5dc5910d63903daaffe53045d8f72
Ccondir <87036822+Ccondir@users.noreply.github.com>
2021-10-30 13:38:41 +0200

    Add files via upload

---
+++ rusianrullet.py
@@ -0,0 +1,59 @@
+import math
+import random
+import time
+
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+if c == rnA:
+    print("you aren't anymore, F")
+    time.sleep(2)
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+if c == rnA:
+    print("you aren't anymore, F")
+    time.sleep(2)
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+if c == rnA:
+    print("you aren't anymore, F")
+    time.sleep(2)
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+if c == rnA:
+    print("you aren't anymore, F")
+    time.sleep(2)
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+if c == rnA:
+    print("you aren't anymore, F")
+    time.sleep(2)
+else:
+    print("congrats you won the game, yayyyyyyyy!!!!")
+    time.sleep(5)
---
+++ russianrullet that will probably crash your PC.py
@@ -0,0 +1,46 @@
+import math
+import random
+import time
+
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while int(c) > 6:
+    print("bruh")
+while c == rnA:
+    print("bruh")
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while c == rnA:
+    print("bruh")
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while c == rnA:
+    print("bruh")
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while c == rnA:
+    print("bruh")
+else:
+    print("you survived this round")
+    time.sleep(2)
+a = ["1", "2", "3", "4,", "5", "6"]
+rnA = random.choice(a)
+c = input("pick a number from 1 to 6: ")
+while c == rnA:
+    print("bruh")
+else:
+    print("congrats you won the game, yayyyyyyyy!!!!")
+    time.sleep(5)
